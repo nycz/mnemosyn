54f8947119231b2d06f4e8e82ed0eac81e06355f
nycz <pyntix@gmail.com>
2013-02-05 14:11:56 +0100

    Changed buttons to QDialogButtonBox in NewTaskDialog

--- newtaskdialog.py
+++ newtaskdialog.py
@@ -14,19 +14,12 @@ class NewTaskDialog(QtGui.QDialog):
         layout.addRow('&Text:', self.text_input)
         layout.addRow('&Description:', self.desc_input)
 
-        button_row = QtGui.QHBoxLayout()
+        button_row = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Save \
+                                            | QtGui.QDialogButtonBox.Cancel)
+        button_row.accepted.connect(self.accept)
+        button_row.rejected.connect(self.reject)
         layout.addRow(button_row)
 
-        ok_btn = QtGui.QPushButton('&OK')
-        ok_btn.setDefault(True)
-        ok_btn.clicked.connect(self.accept)
-        cancel_btn = QtGui.QPushButton('&Cancel')
-        cancel_btn.clicked.connect(self.reject)
-        button_row.addStretch()
-        button_row.addWidget(ok_btn)
-        button_row.addSpacing(40)
-        button_row.addWidget(cancel_btn)
-        button_row.addStretch()
 
         self.setModal(True)
 

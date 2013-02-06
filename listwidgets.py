f18e88b270e743d84c2b07f037dfe2930d5b423e
nycz <pyntix@gmail.com>
2013-02-06 12:14:17 +0100

    Added task sorting buttons (only dummies so far)

--- listwidgets.py
+++ listwidgets.py
@@ -42,14 +42,44 @@ class TagListWidget(ListWidget):
             if not item.isEmpty():
                 item.widget().update_count(tasklist)
 
-class TaskListWidget(QtGui.QWidget):
+
+class TaskListWidget(QtGui.QFrame):
 
     def __init__(self):
         super().__init__()
         self.list_widget = ListWidget()
-        layout = QtGui.QHBoxLayout(self)
-        kill_theming(layout)
-        layout.addWidget(self.list_widget)
+        main_layout = QtGui.QVBoxLayout(self)
+        kill_theming(main_layout)
+        main_layout.addWidget(self.list_widget)
+
+        # ============ Sorting buttons ==============
+        class SortButtonContainer(QtGui.QFrame): pass
+        btn_container = SortButtonContainer(self)
+        btn_layout = QtGui.QHBoxLayout(btn_container)
+        main_layout.addWidget(btn_container)
+        kill_theming(btn_layout)
+
+        class SortButton(QtGui.QPushButton): pass
+        number_sort_btn = SortButton('number')
+        date_sort_btn = SortButton('date')
+
+        number_sort_btn.setCheckable(True)
+        number_sort_btn.setChecked(True)
+        date_sort_btn.setCheckable(True)
+
+        number_sort_btn.setFlat(True)
+        date_sort_btn.setFlat(True)
+
+        btn_group = QtGui.QButtonGroup(self)
+        btn_group.addButton(number_sort_btn)
+        btn_group.addButton(date_sort_btn)
+
+        class SortButtonLabel(QtGui.QLabel): pass
+        btn_layout.addWidget(SortButtonLabel('Sort by:'))
+        btn_layout.addWidget(number_sort_btn)
+        btn_layout.addWidget(date_sort_btn)
+        btn_layout.addStretch()
+        # ===========================================
 
     def add_widget(self, *args):
         self.list_widget.add_widget(*args)
